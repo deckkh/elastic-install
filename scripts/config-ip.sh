e73e43f2b31fddd33bf3c65febdcd89073c02086
Kim Kruse Hansen <deckkh@gmail.com>
2021-12-02 14:50:05 +0100

    more network

--- scripts/config-ip.sh
+++ scripts/config-ip.sh
@@ -1,10 +1,16 @@
 #
 interface="eth0"
+vlan="192.168.0."
+gateway="1"
+mask="24"
 echo -n "enter last octet : "
 read last_octet
-nmcli con show $interface
+echo -n "enter hostname : "
+read hostname
+#nmcli con show $interface
 nmcli con mod $interface ipv4.dns 8.8.8.8
 nmcli con mod $interface +ipv4.dns 8.8.4.4
-nmcli con mod $interface ipv4.gateway 192.168.0.$last_octet
-nmcli con mod $interface ipv4.ipaddresses 192.168.0.2/24
-nmcli con up eth0
+nmcli con mod $interface ipv4.gateway $vlan$gateway
+nmcli con mod $interface ipv4.addresses $vlan$last_octet/$mask
+nmcli general hostname $hostname
+nmcli con up $interface
--- scripts/download-elastic.sh
+++ scripts/download-elastic.sh
@@ -1,6 +1,8 @@
 echo -n "enter elastic version : "
 read version
 
-wget 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-'$version'-x86_64.rpm'
-wget 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-'$version'-x86_64.rpm'
-wget 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-'$version'-x86_64.rpm'
+wget -N 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-'$version'-x86_64.rpm'
+wget -N 'https://artifacts.elastic.co/downloads/kibana/kibana-'$version'-x86_64.rpm'
+wget -N 'https://artifacts.elastic.co/downloads/logstash/logstash-'$version'-x86_64.rpm'
+wget -N 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-'$version'-x86_64.rpm'
+wget -N 'https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-'$version'-x86_64.rpm'
