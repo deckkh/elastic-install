ee4c2e16e056a7b27e3a0ff37d58358af40623e1
Kim Kruse Hansen <deckkh@gmail.com>
2021-12-02 14:05:57 +0100

    all

---
+++ scripts/config-ip.sh
@@ -0,0 +1,6 @@
+nmcli con show "eth0"
+nmcli con mod "eth0" ipv4.dns 8.8.8.8
+nmcli con mod "eth0" +ipv4.dns 8.8.4.4
+nmcli con mod "eth0" ipv4.gateway 192.168.0.1
+nmcli con mod "eth0" ipv4.ipaddresses 192.168.0.2/24
+nmcli con up eth0
---
+++ scripts/download-elastic.sh
@@ -0,0 +1,5 @@
+$version = '7.15.2'
+
+wget 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.15.2-x86_64.rpm'
+wget 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.15.2-x86_64.rpm'
+wget 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.15.2-x86_64.rpm'
