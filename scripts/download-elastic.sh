8327bf5d861278cb835c8bfb98f2b828d0ab56f5
Kim Kruse Hansen <deckkh@gmail.com>
2021-12-03 13:11:31 +0100

    download

--- scripts/download-elastic.sh
+++ scripts/download-elastic.sh
@@ -1,8 +1,8 @@
 echo -n "enter elastic version to download : "
 read version
 
-location='https://artifacts.elastic.co/downloads'
-destdir='/tmp'
+location="https://artifacts.elastic.co/downloads"
+destdir="/tmp"
 
 rm -f $destdir/elasticsearch-*.rpm
 rm -f $destdir/logstash-*.rpm
@@ -12,8 +12,8 @@ rm -f $destdir/metricbeat-*.rpm
 
 
 
-wget -N '$location/elasticsearch/elasticsearch-'$version'-x86_64.rpm' -P $destdir
-wget -N '$location/kibana/kibana-'$version'-x86_64.rpm' -P $destdir
-wget -N '$location/logstash/logstash-'$version'-x86_64.rpm' -P $destdir
-wget -N '$location/beats/filebeat/filebeat-'$version'-x86_64.rpm' -P $destdir
-wget -N '$location/beats/metricbeat/metricbeat-'$version'-x86_64.rpm' -P $destdir
+wget -N $location"/elasticsearch/elasticsearch-"$version"-x86_64.rpm" -P $destdir
+wget -N $location"/kibana/kibana-"$version"-x86_64.rpm" -P $destdir
+wget -N $location"/logstash/logstash-"$version"-x86_64.rpm" -P $destdir
+wget -N $location"/beats/filebeat/filebeat-"$version"-x86_64.rpm" -P $destdir
+wget -N $location"/beats/metricbeat/metricbeat-"$version"-x86_64.rpm" -P $destdir
