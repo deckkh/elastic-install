b5c296f335d92e41ba30fd714fd9164d685e9383
Kim Kruse Hansen <deckkh@gmail.com>
2021-12-04 14:34:39 +0100

    download

--- scripts/download-elastic.sh
+++ scripts/download-elastic.sh
@@ -2,18 +2,20 @@ echo -n "enter elastic version to download : "
 read version
 
 location="https://artifacts.elastic.co/downloads"
-destdir="/tmp"
+destdir="/opt/ansible/elastic-install/files"
+
+mkdir $destdir
 
 rm -f $destdir/elasticsearch-*.rpm
 rm -f $destdir/logstash-*.rpm
 rm -f $destdir/kibana-*.rpm
 rm -f $destdir/filebeat-*.rpm
 rm -f $destdir/metricbeat-*.rpm
-
-
+rm -f $destdir/repository-s3-*.zip
 
 wget -N $location"/elasticsearch/elasticsearch-"$version"-x86_64.rpm" -P $destdir
 wget -N $location"/kibana/kibana-"$version"-x86_64.rpm" -P $destdir
 wget -N $location"/logstash/logstash-"$version"-x86_64.rpm" -P $destdir
 wget -N $location"/beats/filebeat/filebeat-"$version"-x86_64.rpm" -P $destdir
 wget -N $location"/beats/metricbeat/metricbeat-"$version"-x86_64.rpm" -P $destdir
+wget -N $location"elasticsearch-plugins/repository-s3/repository-s3-"$version".zip -P $destdir
