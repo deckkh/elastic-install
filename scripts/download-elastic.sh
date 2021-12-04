423d777ff67e0b86652091be3deb1c0d89597234
Kim Kruse Hansen <deckkh@gmail.com>
2021-12-04 18:19:39 +0100

    GPG

--- scripts/download-elastic.sh
+++ scripts/download-elastic.sh
@@ -12,6 +12,9 @@ rm -f $destdir/kibana-*.rpm
 rm -f $destdir/filebeat-*.rpm
 rm -f $destdir/metricbeat-*.rpm
 rm -f $destdir/repository-s3-*.zip
+rm -f $destdir/GPG-KEY-elasticsearch
+
+wget -N "https://artifacts.elastic.co/GPG-KEY-elasticsearch" -P $destdir
 
 wget -N $location"/elasticsearch/elasticsearch-"$version"-x86_64.rpm" -P $destdir
 wget -N $location"/kibana/kibana-"$version"-x86_64.rpm" -P $destdir
@@ -19,3 +22,4 @@ wget -N $location"/logstash/logstash-"$version"-x86_64.rpm" -P $destdir
 wget -N $location"/beats/filebeat/filebeat-"$version"-x86_64.rpm" -P $destdir
 wget -N $location"/beats/metricbeat/metricbeat-"$version"-x86_64.rpm" -P $destdir
 wget -N $location"/elasticsearch-plugins/repository-s3/repository-s3-"$version".zip" -P $destdir
+
