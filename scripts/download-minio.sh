89e3b4cad215c9342bd5ceb25eb76f2f33fa5499
Kim Kruse Hansen <deckkh@gmail.com>
2021-12-08 20:29:35 +0100

    download-minio

---
+++ scripts/download-minio.sh
@@ -0,0 +1,17 @@
+echo -n "enter minio version to download : "
+read version
+
+location_server="https://dl.min.io/server/minio/release/linux-amd64"
+location_client="https://dl.min.io/client/minio/release/linux-amd64"
+
+destdir="/opt/ansible/elastic-install/files"
+
+mkdir $destdir
+
+rm -f $destdir/minio-*.rpm
+
+
+wget -N $location_server"/minio-20211124231933.0.0.x86_64.rpm" -P $destdir
+wget -N $location_server"/mcli-20211116203736.0.0.x86_64.rpm" -P $destdir
+
+
