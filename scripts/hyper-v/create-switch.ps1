1c899465f4a3273c32d25bef84a2047c0dd9d765
Kim Kruse Hansen <deckkh@gmail.com>
2021-12-20 12:08:39 +0100

    create-switch

--- scripts/hyper-v/create-switch.ps1
+++ scripts/hyper-v/create-switch.ps1
@@ -5,6 +5,8 @@ $prefix = 24
 
 # read docs at https://docs.microsoft.com/en-us/virtualization/hyper-v-on-windows/user-guide/setup-nat-network
 
+# these commands will create a new NAT switch in your Hyper-v host to be able to have static IP's on your VM's
+
 New-VMSwitch -SwitchName $name -SwitchType Internal
 $adapter = get-netadapter -name "vEthernet ($name)"
 New-NetIPAddress -IPAddress $gateway -PrefixLength $prefix -InterfaceIndex $adapter.ifIndex
