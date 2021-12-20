c44e8349688ff4ef19666cdb09631fc44f099871
Kim Kruse Hansen <deckkh@gmail.com>
2021-12-20 18:49:17 +0100

    kafka

---
+++ scripts/download-kafka.sh
@@ -0,0 +1,14 @@
+echo -n "enter kafka version to download : "
+read version
+
+location_server="https://archive.apache.org/dist/kafka/"+$version+"/kafka_2.13-"+$version"+.tgz"
+
+destdir="/opt/ansible/elastic-install/files"
+
+mkdir $destdir
+
+rm -f $destdir/kafka*
+
+wget -N $location_server -P $destdir
+
+
