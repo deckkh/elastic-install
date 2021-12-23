20a72f2f33b17aef2df4ab4061d6da9a7e4f2014
Kim Kruse Hansen <deckkh@gmail.com>
2021-12-23 18:47:20 +0100

    easyrsa

---
+++ scripts/download-easyrsa.sh
@@ -0,0 +1,14 @@
+echo -n "enter easyrsa version to download : "
+read version
+
+location_server="https://github.com/OpenVPN/easy-rsa/releases/download/"$version"/EasyRSA-"$version".tgz"
+
+destdir="/opt/ansible/elastic-install/files"
+
+mkdir $destdir
+
+rm -f $destdir/easyrsa*.tgz
+
+wget -N $location_server -P $destdir
+
+
